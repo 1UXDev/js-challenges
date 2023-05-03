ed486e0eb6961b87a3c09445e796fb1b69e2f465
mntzd <julien.leske@gmail.com>
2023-05-03 15:19:26 +0200

    challenges done

---
+++ js-loops_forin-select-nav/js/index.js
@@ -0,0 +1,54 @@
+console.clear();
+
+const main = document.querySelector('[data-js="main"]');
+
+// Part 1: Creating a Select Input
+
+const languages = {
+  DE: "German",
+  EN: "English",
+  ES: "Spanish",
+  FR: "French",
+  IT: "Italian",
+  NL: "Dutch",
+  PT: "Portuguese",
+  RU: "Russian",
+  ZH: "Chinese",
+};
+
+const select = document.createElement("select");
+select.name = "languages";
+main.append(select);
+
+// --v-- write/change code here --v--
+for (language in languages) {
+  let option = document.createElement("option");
+  option.innerText = languages[language];
+  select.append(option);
+}
+// --^-- write/change code here --^--
+
+// Part 2: Creating a Navigation Bar
+
+const nav = {
+  home: { href: "/home", text: "Home" },
+  about: { href: "/about", text: "About" },
+  contact: { href: "/contact", text: "Contact" },
+};
+
+const navElement = document.createElement("nav");
+const ul = document.createElement("ul");
+main.append(navElement);
+navElement.append(ul);
+
+// --v-- write/change code here --v--
+for (item in nav) {
+  let li = document.createElement("li");
+  let a = document.createElement("a");
+  a.setAttribute("href", nav[item].href);
+  a.innerText = nav[item].text;
+
+  ul.append(li);
+  li.append(a);
+}
+// --^-- write/change code here --^--

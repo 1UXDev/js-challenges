379fe13c9a58e2b610a398fe1fbfda671e125e76
mntzd <julien.leske@gmail.com>
2023-05-04 16:04:43 +0200

    array-challenges

---
+++ arrays/SPA/js-array-methods_spa-3/js/index.js
@@ -0,0 +1,14 @@
+const allPages = document.querySelectorAll('[data-js*="page"]');
+const allLinks = document.querySelectorAll('[data-js*="link"]');
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    allPages.forEach((page) => {
+      page.classList.remove("current");
+    });
+    const hrefAttribute = event.target.getAttribute("href");
+    //console.log(event.target);
+    const currentPage = document.querySelector(hrefAttribute);
+    currentPage.classList.add("current");
+  });
+});
